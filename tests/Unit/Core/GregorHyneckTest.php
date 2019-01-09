81a6b34e5be7f520951dd9e584d6d18ff16f5e50
Gregor Hyneck <gregor.hyneck@oxid-esales.com>
2019-01-09 15:31:01 +0100

    OXDEV-1731 Test if test suite on CI gets red if exit() is hit

---
+++ tests/Unit/Core/GregorHyneckTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+namespace OxidEsales\EshopCommunity\Tests\Unit\Core;
+
+use OxidEsales\TestingLibrary\UnitTestCase;
+
+class GregorHyneckTest extends UnitTestCase
+{
+    public function testGh()
+    {
+        exit('Will test suite be red on CI?' . PHP_EOL);
+    }
+}
